3e30abbeb485d169ca7abc993646d131a0aaaecf
Zuzana Stichova <z.stichova@gmail.com>
2020-10-07 15:02:35 +0200

    Intermediate part 2

--- Intermediate/file.js
+++ Intermediate/file.js
@@ -5,8 +5,22 @@ console.table(dataProduct);
 
 $(document).ready(function()
 {
-    $('img[src*=notebook]').parent().parent().parent().parent().css('background-color','red');
+    $('img[src*=notebook]').parents(".prod-info-main").css('background-color','red');
     $('img[src*=tablet]').parent().parent().parent().parent().css('background-color','blue');
     $('img[src*=phone]').parent().parent().parent().parent().css('background-color','green');
 
+    for (let i = 0 ; i < dataProduct.length ; i++) {
+        $('.btn-info:eq('+i+')').after(`<p id=${i}>${dataProduct[i].quantity}</p>`);
+    } 
+
+    $('.btn-danger').on('click',function()
+    {
+        var x = $(this).siblings('p').attr('id');
+        console.log(x);
+        var qtty = Number(dataProduct[x].quantity) - 1;
+        console.log("input is " + dataProduct[x].quantity);
+        $(`#${x}`).text(qtty);
+        dataProduct[x].quantity = qtty;
+        console.log("final is " + dataProduct[x].quantity);
+    })
 })
